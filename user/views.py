f00aa0b0f854db241282d88394d7632147ea498c
lowjack1 <harshit11541@gmail.com>
2020-10-11 19:47:57 +0530

    fix ui issue

--- user/views.py
+++ user/views.py
@@ -3,22 +3,6 @@ from django.http import HttpResponse
 from .models import User, Room , House
 from django.template import loader
 
-def index(request):
-	user_list = User.objects.order_by('name')
-	template = loader.get_template('user/index.html')
-	context = {
-		'user_list':user_list,
-	}
-	return HttpResponse(template.render(context, request))
-
-def home(request):
-	return render(request,'user/home.html')
-
-def about(request):
-	return render(request,'user/about.html')
-
-def contact(request):
-	return render(request,'user/contact.html')
 
 def profile(request):
 	emil = request.session['member_id']
@@ -32,10 +16,3 @@ def profile(request):
 def post(request):
 	user = request.session['member_id']
 	return render(request, 'user/post.html', {'user':user})
-
-def logout(request):
-	try:
-		del request.session['member_id']
-	except KeyError:
-		pass
-	return render(request, 'index.html')
