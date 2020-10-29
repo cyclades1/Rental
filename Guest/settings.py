aaf8deb7ece427a77c7f6a095fa1748756168781
cyclades1 <abhishekdubey007111@gmail.com>
2020-10-29 14:18:43 +0530

    debigging is false

--- Guest/settings.py
+++ Guest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$$ac@i2xro*#8(t8kot@@4-ev^zcued9(y4=z=3goc$_2i+n@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
